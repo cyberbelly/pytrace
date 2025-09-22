ee8930f709a6c2f65b1a8cbdf2f95d29479fbd83
belly <67766715+cyberbelly@users.noreply.github.com>
2025-09-22 19:57:42 -0400

    Update pytrace.py

--- pytrace.py
+++ pytrace.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 up_arrow   = '\u2191'
 down_arrow = '\u2193'
-website_dic={'website name':'list of ips'}
+website_dic={'website name':'list of ips'} # input the website name(used for printing ) and a list/tupal used for tracerouting
 traceroute ='traceroute -Tn {}'
 today      = datetime.strftime(datetime.today(),'%d%b%y')
 log_file   = f'{today}_fulllogs.txt'
@@ -21,7 +21,7 @@ def list_to_dic(traces_list):
     for trace in filter(None,traces_list[1:]):
         print(trace)
         hop,ip=trace.split('  ')[:2]
-        if re.match('(\d{,3}\.){3}\d{,3}',ip):
+        if re.match(r'(\d{,3}\.){3}\d{,3}',ip):
             hop=int(hop)
             trace_dic.update({hop:ip})
     return trace_dic
